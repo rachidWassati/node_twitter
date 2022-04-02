b67e43a3d69a5206582be829d7ab8cd664eacbe5
rachidj <rachidj@protonmail.ch>
2022-04-02 21:50:32 +0200

    creation de notre formulaire de tweet

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
 
+router.get('/tweet/new', (req, res) => {
+    res.render('tweets/tweet-form');
+})
+
 router.get('/', (req, res) => {
     res.render('tweets/tweet-list');
 })
